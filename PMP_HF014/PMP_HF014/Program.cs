608f462b89211de830726b12c8c0a6a66a66deb9
Vajdani <50171679+Vajdani@users.noreply.github.com>
2025-11-26 15:20:27 +0100

    Update Program.cs

--- PMP_HF014/PMP_HF014/Program.cs
+++ PMP_HF014/PMP_HF014/Program.cs
@@ -2,54 +2,53 @@
 {
     internal class Program
     {
+        static long instructionIndex;
+        static string[] program;
+
+        static void Output(long count)
+        {
+            StreamWriter writer = new("output.txt");
+            writer.Write(count);
+            Console.Write(count);
+            writer.Close();
+        }
+
+        static int RecurseProgram(string terminator)
+        {
+            int pathCount = 1;
+            while (program[instructionIndex] != terminator)
+            {
+                if (program[instructionIndex] == "if")
+                {
+                    instructionIndex++;
+                    pathCount *= RecurseProgram("else") + RecurseProgram("endif");
+                }
+
+                instructionIndex++;
+            }
+
+            return pathCount;
+        }
+
         static void Main(string[] args)
         {
             StreamReader reader = new("input.txt");
-            if (!long.TryParse(reader.ReadLine(), out long rows))
+            long rows = long.Parse(reader.ReadLine());
+
+            program = new string[rows];
+            for (int i = 0; i < rows; i++)
             {
-                throw new Exception("Program length is not parseable as a number");
+                program[i] = reader.ReadLine();                
             }
 
-            long rowCount = 0;
-            long pathCount = 0;
-            while (!reader.EndOfStream)
+            if (rows == 2 && program[0] == "begin" && program[1] == "end")
             {
-                rowCount++;
-                if (rowCount > rows)
-                {
-                    throw new Exception("Program length is larger than declared");
-                }
-
-                string line = reader.ReadLine();
-                if (rowCount == 1 && line != "begin" || rowCount == rows && line != "end")
-                { 
-                    throw new Exception("Invalid program");
-                }
-
-                if (line == "if")
-                {
-                    pathCount++;
-                }
-                else if(line == "else")
-                {
-                    pathCount++;
-                    rowCount++;
-                    reader.ReadLine();
-                }
-            }
-            reader.Close();
-
-            if (rowCount < rows)
-            {
-                throw new Exception("Program length is shorter than declared");
+                Output(1);
+                return;
             }
 
-            if (rowCount == 2)
-            {
-                pathCount++;
-            }
-
-            Console.WriteLine(pathCount);
+            instructionIndex = 0;
+            Output(RecurseProgram("end"));
         }
     }
 }
